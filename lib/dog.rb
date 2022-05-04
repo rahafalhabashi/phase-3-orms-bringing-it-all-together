add9707aad561c4d80ace7e50a635487a89bacad
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-05-04 09:27:47 -0400

    commit tree 40b1bf69b0e61f5745e334a36b0da9220978366b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,37 @@
 class Dog
+  attr_accessor :name, :breed, :id
 
+  def initialize(name:, breed:, id: nil)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+        DROP TABLE IF EXISTS dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    self
+  end
 end
